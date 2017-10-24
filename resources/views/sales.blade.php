2e5c0137679e4b95808afc1d7d327511d02d0318
sanjay <sanjay@patchofland.com>
2017-10-23 18:08:21 -0700

    CAP : Token

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -95,6 +95,7 @@
 
                 <script src="https://checkout-sandbox.getbread.com/bread.js" data-api-key="6e60f090-d169-447d-939b-2cb54941d7aa"></script>
                 <form id="bread-checkout-form" action="{{ url('/confirm') }}" method="POST">
+                    {{ csrf_field() }}
                     <div id="bread-checkout-btn" data-bread-default-size="true"></div>
                     <input type="hidden" name="fullname" id="funame">
                     <input type="hidden" name="address" id="address">
